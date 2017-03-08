0e28e675535617a8de3c3fd2deed092a1cf753be
BigMurry <hhdiao@siom.ac.cn>
2017-03-08 17:54:06 +0800

    fix regex bug

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ var urlTwister = require('url-twister');
 var path = require('path');
 var gutil = require('gulp-util');
 
-var replacer = function replacer (str, options) {
-  var filter = options.filter;
-  var twisterOpt = {
-    prepend: options.prepend,
-    root: options.root,
-    append: options.append
-  };
-  var reg = /['"]?([^'"]+\.(?:png|gif|jpg|svg|ico|eot|ttf|woff|otf))['"]?/g;
+var PluginName = 'gulp-js-url-adjuster';
+var REG = /(?:['"]|url *\() *([^ ()'"]+\.(?:png|gif|jpg|svg|ico|eot|ttf|woff|otf)) *[)'"]/;
+
+var replacer = function replacer (str, opt) {
+  var filter = opt.filter;
+  var reg = new RegExp(REG, 'g');
   var m = reg.exec(str);
   var urls = {};
   while (m) {
     if ((typeof filter === 'function' && filter(m[1])) ||
       (filter instanceof RegExp && filter.test(m[1]))) {
-      urls[m[1]] = urlTwister(m[1], null, twisterOpt);
+      urls[m[1]] = urlTwister(m[1], null, opt.twisterOpt);
     }
     m = reg.exec(str);
   }
@@ -27,14 +25,23 @@ var replacer = function replacer (str, options) {
 };
 
 module.exports = function (options) {
+  options = options || {};
+  var replacerOpt = {
+    filter: options.filter,
+    twisterOpt: {
+      prepend: options.prepend,
+      root: options.root,
+      append: options.append
+    }
+  };
+
   return through.obj(function (file, enc, cb) {
     if (path.extname(file.path) !== '.js') {
-      throw new gutil.PluginError('gulp-js-url-adjuster', `[${file.path}] is not a js file.`);
+      throw new gutil.PluginError(PluginName, `[${file.path}] is not a js file.`);
     }
-    options = options || {};
     var js = falafel(file.contents.toString(), function (node) {
-      if (node.type === 'Literal') {
-        node.update(replacer(node.source(), options));
+      if (node.type === 'Literal' && REG.test(node.source())) {
+        node.update(replacer(node.source(), replacerOpt));
       }
     });
     file.contents = new Buffer(js.toString());
